704e817ab99059c55f9ba6746d83a18ec2f9ed02
Michał Sztymelski <166327@student.uwm.edu.pl>
2023-10-31 09:25:20 +0100

    uuh

--- zaj3_warun/Form1.Designer.cs
+++ zaj3_warun/Form1.Designer.cs
@@ -45,27 +45,29 @@
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(192, 63);
+            textBox1.Location = new Point(168, 47);
+            textBox1.Margin = new Padding(3, 2, 3, 2);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(125, 27);
+            textBox1.Size = new Size(110, 23);
             textBox1.TabIndex = 0;
             textBox1.TextChanged += textBox1_TextChanged;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(192, 126);
+            label1.Location = new Point(168, 94);
             label1.Name = "label1";
-            label1.Size = new Size(50, 20);
+            label1.Size = new Size(38, 15);
             label1.TabIndex = 1;
             label1.Text = "label1";
             label1.Click += label1_Click;
             // 
             // button1
             // 
-            button1.Location = new Point(98, 337);
+            button1.Location = new Point(86, 253);
+            button1.Margin = new Padding(3, 2, 3, 2);
             button1.Name = "button1";
-            button1.Size = new Size(94, 68);
+            button1.Size = new Size(82, 51);
             button1.TabIndex = 2;
             button1.Text = "\r\nSB";
             button1.TextAlign = ContentAlignment.TopCenter;
@@ -74,36 +76,43 @@
             // 
             // button2
             // 
-            button2.Location = new Point(223, 337);
+            button2.Location = new Point(195, 253);
+            button2.Margin = new Padding(3, 2, 3, 2);
             button2.Name = "button2";
-            button2.Size = new Size(94, 68);
+            button2.Size = new Size(82, 51);
             button2.TabIndex = 3;
             button2.Text = "SS";
             button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
             // 
             // button3
             // 
-            button3.Location = new Point(341, 337);
+            button3.Location = new Point(298, 253);
+            button3.Margin = new Padding(3, 2, 3, 2);
             button3.Name = "button3";
-            button3.Size = new Size(94, 68);
+            button3.Size = new Size(82, 51);
             button3.TabIndex = 4;
             button3.Text = "SI";
             button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
             // 
             // button4
             // 
-            button4.Location = new Point(463, 337);
+            button4.Location = new Point(405, 253);
+            button4.Margin = new Padding(3, 2, 3, 2);
             button4.Name = "button4";
-            button4.Size = new Size(94, 68);
+            button4.Size = new Size(82, 51);
             button4.TabIndex = 5;
             button4.Text = "SM";
             button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
             // 
             // button5
             // 
-            button5.Location = new Point(591, 337);
+            button5.Location = new Point(517, 253);
+            button5.Margin = new Padding(3, 2, 3, 2);
             button5.Name = "button5";
-            button5.Size = new Size(94, 68);
+            button5.Size = new Size(82, 51);
             button5.TabIndex = 6;
             button5.Text = "SQ";
             button5.UseVisualStyleBackColor = true;
@@ -111,25 +120,28 @@
             // checkBox1
             // 
             checkBox1.AutoSize = true;
-            checkBox1.Location = new Point(377, 69);
+            checkBox1.Location = new Point(330, 52);
+            checkBox1.Margin = new Padding(3, 2, 3, 2);
             checkBox1.Name = "checkBox1";
-            checkBox1.Size = new Size(18, 17);
+            checkBox1.Size = new Size(15, 14);
             checkBox1.TabIndex = 7;
             checkBox1.UseVisualStyleBackColor = true;
             checkBox1.CheckedChanged += checkBox1_CheckedChanged;
             // 
             // numericUpDown1
             // 
-            numericUpDown1.Location = new Point(463, 64);
+            numericUpDown1.Location = new Point(405, 48);
+            numericUpDown1.Margin = new Padding(3, 2, 3, 2);
             numericUpDown1.Name = "numericUpDown1";
-            numericUpDown1.Size = new Size(150, 27);
+            numericUpDown1.Size = new Size(131, 23);
             numericUpDown1.TabIndex = 8;
             // 
             // button6
             // 
-            button6.Location = new Point(489, 122);
+            button6.Location = new Point(428, 92);
+            button6.Margin = new Padding(3, 2, 3, 2);
             button6.Name = "button6";
-            button6.Size = new Size(94, 29);
+            button6.Size = new Size(82, 22);
             button6.TabIndex = 9;
             button6.Text = "button6";
             button6.UseVisualStyleBackColor = true;
@@ -137,26 +149,26 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(463, 188);
+            label2.Location = new Point(405, 141);
             label2.Name = "label2";
-            label2.Size = new Size(50, 20);
+            label2.Size = new Size(38, 15);
             label2.TabIndex = 10;
             label2.Text = "label2";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(563, 188);
+            label3.Location = new Point(493, 141);
             label3.Name = "label3";
-            label3.Size = new Size(50, 20);
+            label3.Size = new Size(38, 15);
             label3.TabIndex = 11;
             label3.Text = "label3";
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(700, 338);
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(button6);
@@ -169,6 +181,7 @@
             Controls.Add(button1);
             Controls.Add(label1);
             Controls.Add(textBox1);
+            Margin = new Padding(3, 2, 3, 2);
             Name = "Form1";
             Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
--- zaj3_warun/Form1.cs
+++ zaj3_warun/Form1.cs
@@ -1,3 +1,5 @@
+using System.CodeDom.Compiler;
+
 namespace zaj3_warun
 {
     public partial class Form1 : Form
@@ -7,38 +9,236 @@ namespace zaj3_warun
             InitializeComponent();
         }
 
-        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        public void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
 
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-            
-        }
-
-
-
-        private void label1_Click(object sender, EventArgs e)
+        public void textBox1_TextChanged(object sender, EventArgs e)
         {
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
+
+
+        public void label1_Click(object sender, EventArgs e)
         {
-            //string liczby = textBox1.Text;
-            //liczby.Split(' ');
-            //label1.Text = liczby;
+
         }
-        int[] Converty(string napis)
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+            string liczby = textBox1.Text;
+            int[] uuh = TextToIntTable(liczby);
+
+            uuh = SortBoubble(uuh);
+
+            string porvalo = IntTableToString(uuh);
+            label1.Text = porvalo;
+        }
+
+        int[] SortBoubble(int[] lista)
+        {
+            int issorted = 0;
+
+            while (issorted != lista.Length - 1)
+            {
+                for (int i = 0; i < lista.Length - 1; i++)
+                {
+                    if (lista[i] <= lista[i + 1])
+                    {
+                        issorted++;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+
+                }
+                if (issorted < lista.Length - 1)
+                    issorted = 0;
+                for (int i = 0; i < lista.Length - 1; i++)
+                {
+                    if (lista[i] > lista[i + 1])
+                    {
+                        int temp = lista[i];
+                        lista[i] = lista[i + 1];
+                        lista[i + 1] = temp;
+                    }
+                }
+            }
+            return lista;
+        }
+
+        string IntTableToString(int[] uuh)
+        {
+            string[] tekst = new string[uuh.Length];
+            //label1.Text = uuh.ToString();
+            for (int i = 0; i < uuh.Length; i++)
+            {
+                tekst[i] = uuh[i].ToString();
+            }
+            string porvalo = string.Join(' ', tekst);
+            return porvalo;
+        }
+        int[] TextToIntTable(string napis)
         {
             var LiczbyS = napis.Trim().Split(' ');
             var liczby = new int[LiczbyS.Length];
-            for(int i = 0; i < LiczbyS.Length; i++)
+            for (int i = 0; i < LiczbyS.Length; i++)
             {
                 liczby[i] = int.Parse(LiczbyS[i]);
             }
             return liczby;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string liczby = textBox1.Text;
+            int[] uuh = TextToIntTable(liczby);
+
+            uuh = SortSelection(uuh);
+
+            string porvalo = IntTableToString(uuh);
+            label1.Text = porvalo;
+        }
+
+
+        int[] SortSelection(int[] lista)
+        {
+            int issorted = 0;
+            int minimum = lista[0];
+
+            while (issorted != lista.Length - 1)
+            {
+                for (int i = 0; i < lista.Length - 1; i++)
+                {
+                    if (lista[i] <= lista[i + 1])
+                    {
+                        issorted++;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+
+                }
+                if (issorted < lista.Length - 1)
+                    issorted = 0;
+                for (int i = 0; i < lista.Length; i++)
+                {
+                    for (int j = i + 1; j < lista.Length; j++)
+                    {
+                        if (lista[i] > lista[j])
+                        {
+                            int temp = lista[i];
+                            lista[i] = lista[j];
+                            lista[j] = temp;
+                            break;
+                        }
+                    }
+
+                }
+            }
+
+            return lista;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string liczby = textBox1.Text;
+            int[] uuh = TextToIntTable(liczby);
+
+            uuh = SortInsertion(uuh);
+
+            string porvalo = IntTableToString(uuh);
+            label1.Text = porvalo;
+        }
+
+        int[] SortInsertion(int[] lista)
+        {
+            int issorted = 0;
+            int minimum = lista[0];
+
+            while (issorted != lista.Length - 1)
+            {
+                for (int i = 0; i < lista.Length - 1; i++)
+                {
+                    if (lista[i] <= lista[i + 1])
+                    {
+                        issorted++;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+
+                }
+                if (issorted < lista.Length - 1)
+                    issorted = 0;
+                for (int i = 0; i < lista.Length - 1; i++)
+                {
+                    if (lista[i] > lista[i + 1])
+                    {
+                        for (int j = i; j >= 0; j--)
+                        {
+                            int temp = lista[j];
+                            lista[j] = lista[j + 1];
+                            lista[j + 1] = temp;
+                            if (lista[j] < lista[i])
+                            {
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+
+            return lista;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string liczby = textBox1.Text;
+            int[] uuh = TextToIntTable(liczby);
+
+            uuh = SortMerge(uuh);
+
+            string porvalo = IntTableToString(uuh);
+            label1.Text = porvalo;
+        }
+
+        int[] SortMerge(int[] lista)
+        {
+            int issorted = 0;
+
+            while (issorted != lista.Length - 1)
+            {
+                for (int i = 0; i < lista.Length - 1; i++)
+                {
+                    if (lista[i] <= lista[i + 1])
+                    {
+                        issorted++;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+
+                }
+                if (issorted < lista.Length - 1)
+                    issorted = 0;
+                for (int i = 0; i < lista.Length - 1; i++)
+                {
+                    if (lista[i] > lista[i + 1])
+                    {
+                        int temp = lista[i];
+                        lista[i] = lista[i + 1];
+                        lista[i + 1] = temp;
+                    }
+                }
+            }
+            return lista;
         }
     }
 }
